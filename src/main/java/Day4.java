c90f2d49e0aba57f3a3a4119a8864ccdd212c141
Martin Bergström <emarbe02@gmail.com>
2022-12-05 22:14:02 +0100

    Done day4

--- src/main/java/Day4.java
+++ src/main/java/Day4.java
@@ -1,19 +1,30 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Day4 {
     public static void main(String[] args) throws Exception {
         System.out.println(solvePart1());
+        System.out.println(solvePart2());
     }
 
     private static long solvePart1() throws Exception {
         return Files.lines(Paths.get(Day4.class.getResource("Day4.txt").toURI()))
-                .filter(Day4::isOverlapping)
+                .map(Day4::createPair)
+                .filter(Day4::isOverlappingCompletely)
                 .count();
     }
 
-    private static boolean isOverlapping(String line) {
+    private static long solvePart2() throws Exception {
+        return Files.lines(Paths.get(Day4.class.getResource("Day4.txt").toURI()))
+                .map(Day4::createPair)
+                .filter(Day4::isOverlappingPartially)
+                .count();
+    }
+
+    private static Pair<List<Integer>> createPair(String line) {
         String sectionA = line.split(",")[0];
         int startA = Integer.parseInt(sectionA.split("-")[0]);
         int endA = Integer.parseInt(sectionA.split("-")[1]);
@@ -22,13 +33,37 @@ public class Day4 {
         int startB = Integer.parseInt(sectionB.split("-")[0]);
         int endB = Integer.parseInt(sectionB.split("-")[1]);
 
-        boolean isAOverlappedByB =
-                IntStream.range(startA, endA).allMatch(numberA -> IntStream.range(startB, endB).anyMatch(numberB -> numberB == numberA));
+        List<Integer> rangeA = IntStream.range(startA, (endA + 1)).boxed().collect(Collectors.toList());
+        List<Integer> rangeB = IntStream.range(startB, (endB + 1)).boxed().collect(Collectors.toList());
+        return new Pair<>(rangeA, rangeB);
+    }
 
-        boolean isBOverlappedByA =
-                IntStream.range(startB, endB).allMatch(numberB -> IntStream.range(startA, endA).anyMatch(numberA -> numberA == numberB));
+    private static boolean isOverlappingCompletely(Pair<List<Integer>> pair) {
+        return isRangeFullyOverlappingOtherRange(pair.first, pair.second) ||
+                isRangeFullyOverlappingOtherRange(pair.second, pair.first);
+    }
 
-        return isAOverlappedByB || isBOverlappedByA;
+    private static boolean isOverlappingPartially(Pair<List<Integer>> pair) {
+        return isRangeOverlappingOtherRange(pair.first, pair.second) ||
+                isRangeOverlappingOtherRange(pair.second, pair.first);
+    }
+
+    private static boolean isRangeFullyOverlappingOtherRange(List<Integer> rangeA, List<Integer> rangeB) {
+        return rangeA.stream().allMatch(numberA -> rangeB.stream().anyMatch(numberB -> numberB.equals(numberA)));
+    }
+
+    private static boolean isRangeOverlappingOtherRange(List<Integer> rangeA, List<Integer> rangeB) {
+        return rangeA.stream().anyMatch(numberA -> rangeB.stream().anyMatch(numberB -> numberB.equals(numberA)));
+    }
+
+    private static class Pair<T> {
+        T first;
+        T second;
+
+        Pair(T first, T second) {
+            this.first = first;
+            this.second = second;
+        }
     }
 
 }
